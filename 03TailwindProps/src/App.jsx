b4c30d7dd7f638acab23ec049653b7ef127ffd02
Akash Chandole <65582164+thechandole@users.noreply.github.com>
2025-12-14 14:46:02 +0530

    Add files via upload

---
+++ 03TailwindProps/src/App.jsx
@@ -0,0 +1,14 @@
+import {CardImage} from './components/Card'
+
+function App() {
+
+
+  return (
+    <>
+      <h1 className='bg-amber-500 p-4 rounded-2xl mb-3'> Hello World !!</h1>
+      <CardImage   btnText = "padhtey rahe....."/>
+    </>
+  )
+}
+
+export default App
